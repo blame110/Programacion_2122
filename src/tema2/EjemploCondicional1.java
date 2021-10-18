c6c9901b055173ebffc16c72240b279195763a51
blame110 <pmpinformatica@gmail.com>
2021-10-18 11:11:48 +0200

    Actualización 18 octubre

--- src/tema2/EjemploCondicional1.java
+++ src/tema2/EjemploCondicional1.java
@@ -50,6 +50,10 @@ public class EjemploCondicional1 {
 		// El nombre no puede tener más de 8 caracteres
 		if (nombre.length() > 8) {
 			System.out.println("El nombre no puede tener más de 8 carácteres");
+
+			if (nombre.isEmpty()) {
+				System.out.println("No puede estar vacio");
+			}
 		} else
 		// Este código se ejecuta cuando la condicion del if es falsa
 		{
@@ -57,9 +61,53 @@ public class EjemploCondicional1 {
 			char cInicial = nombre.charAt(0);
 
 			// Comprobamos que ese caracter no es un numero
+			// if (Character.isDigit(cInicial)) es lo mismo que lo de abajo
+
 			if (cInicial == '0' || cInicial == '1' || cInicial == '2' || cInicial == '3' || cInicial == '4'
 					|| cInicial == '5' || cInicial == '6' || cInicial == '7' || cInicial == '8' || cInicial == '9') {
 				System.out.println("El primer carácter del nombre no puede ser un número");
+			}
+		}
+
+		// El password tiene que cumplir mas de 10 caracteres, y que empiece por un 1
+		if (password.length() > 10 && password.charAt(0) == '1')
+			System.out.println("El password cumple las condiciones");
+
+		// De extra el empleado cobra 100 euros de base
+		// si es soltero y menor de 40 años cobra ademas 200 euros
+		// Los mayores de 40 cobraran 400 euros
+		// Para los menores:
+		// Los divorciados mayores de 30 años o que tengan sueldo inferior a 1000 euros
+		// cobraran
+		// 500
+		// A los zombies les quitamos 2000 euros para que se vayan a la competencia
+		int extra = 100;
+
+		// Comprobamos si es menor de 40 años y si es soltero, pasamos a minusculas para
+		// que
+		// no haya problemas de mayusculas y minusculas
+
+		if (edad > 40) {
+			// Incrementamos la extra en 400
+			// Es igual que poner extra=extra+400;
+			extra += 400;
+		} else {
+			// La edad es menor de 40
+			if (estadoCivil.toLowerCase().equals("soltero")) {
+				// Se le suma 200 euros a lo que tenía la extra
+				extra = extra + 200;
+			}
+
+			if (estadoCivil.toUpperCase().equals("DIVORCIADO") && (edad > 30 || sueldo < 1000))
+			// ejemplo sueldo 1010 edad 22 estado soltero cierto si se hace primero el &&
+			// Si se hace primero el || seria falso
+			{
+				extra = extra + 500;
+			}
+
+			if (estadoCivil.toUpperCase().equals("ZOMBIE")) {
+				extra = extra - 2000;
+				// extra-=2000;
 			}
 		}
 
---
+++ src/tema2/EjemploCondicional2.java
@@ -0,0 +1,26 @@
+package tema2;
+
+public class EjemploCondicional2 {
+
+	public static final int PORCENTAJE_BECA6000 = 30;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// El programa leera por teclado la nota de un alumno, su nombre, edad y curso
+		// Mostrara SUSPENSO, SUFICIENTE, BIEN, NOTABLE, SOBRESALIENTE dependiendo de la
+		// nota
+
+		// Si es de tercero y tiene sobresaliente o es mayor de edad se mostrará el
+		// mensaje
+		// "APROBADO CON EXCELENCIA"
+
+		// A todos los que tengan sobresaliente mayores de 14 años tendrán beca
+
+		// se pide que el código este bien comentado y las variables con los nombres
+		// adecuados
+		// en el formato adecuado, por ejemplo notaAlumno
+
+	}
+
+}
