ec4b0eb710c28deb846e545682151e96f100dffe
blame110 <pmpinformatica@gmail.com>
2021-10-19 12:12:27 +0200

    Actualización 19 oct

--- src/tema2/EjemploCondicional2.java
+++ src/tema2/EjemploCondicional2.java
@@ -1,5 +1,7 @@
 package tema2;
 
+import javax.swing.JOptionPane;
+
 public class EjemploCondicional2 {
 
 	public static final int PORCENTAJE_BECA6000 = 30;
@@ -7,7 +9,8 @@ public class EjemploCondicional2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// El programa leera por teclado la nota de un alumno, su nombre, edad y curso
+		// El programa leera por teclado la nota de un alumno que sera un float, su
+		// nombre, edad y curso
 		// Mostrara SUSPENSO, SUFICIENTE, BIEN, NOTABLE, SOBRESALIENTE dependiendo de la
 		// nota
 
@@ -20,6 +23,14 @@ public class EjemploCondicional2 {
 		// se pide que el código este bien comentado y las variables con los nombres
 		// adecuados
 		// en el formato adecuado, por ejemplo notaAlumno
+
+		JOptionPane.showMessageDialog(null, "Ventana de Mensajes", "Este es el Mensaje",
+				JOptionPane.INFORMATION_MESSAGE);
+
+		String nombre = JOptionPane.showInputDialog(null, "Nombre", "Introduce tu nombre",
+				JOptionPane.QUESTION_MESSAGE);
+
+		System.out.println("Tu nombre es: " + nombre);
 
 	}
 
