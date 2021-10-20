74b62bf46a7a979901ec3d07cb01ad3c1c147231
blame110 <pmpinformatica@gmail.com>
2021-10-20 10:39:45 +0200

    Actualización 20 octubre

--- src/tema2/EjemploCondicional2.java
+++ src/tema2/EjemploCondicional2.java
@@ -24,13 +24,80 @@ public class EjemploCondicional2 {
 		// adecuados
 		// en el formato adecuado, por ejemplo notaAlumno
 
-		JOptionPane.showMessageDialog(null, "Ventana de Mensajes", "Este es el Mensaje",
+		// Declaración de variables
+		float notaAlumno = 0;
+		String nombre = "";
+		int edad = 0;
+		String curso = "";
+		String sEdad = "", sNota = "";
+		// Mensaje resultado del programa
+		String mensaje = "";
+
+		// JOptionPane.showMessageDialog(null, "Ventana de Mensajes", "Este es el
+		// Mensaje",
+		// JOptionPane.INFORMATION_MESSAGE);
+
+		nombre = JOptionPane.showInputDialog(null, "Nombre", "Introduce tu nombre", JOptionPane.QUESTION_MESSAGE);
+
+		sNota = JOptionPane.showInputDialog(null, "Introduce tu nota en número:", "Nota", JOptionPane.PLAIN_MESSAGE);
+
+		// Convertimos el String a float
+		notaAlumno = Float.parseFloat(sNota);
+
+		sEdad = JOptionPane.showInputDialog(null, "Introduce tu edad:", "Edad", JOptionPane.INFORMATION_MESSAGE);
+
+		// Convertimos la edad a int
+		edad = Integer.parseInt(sEdad);
+
+		curso = JOptionPane.showInputDialog(null, "Introduce el Curso en el que estas matriculado:", "Curso",
 				JOptionPane.INFORMATION_MESSAGE);
 
-		String nombre = JOptionPane.showInputDialog(null, "Nombre", "Introduce tu nombre",
-				JOptionPane.QUESTION_MESSAGE);
+		// System.out.println(nombre + "-" + notaAlumno + "-" + edad + "-" + curso);
 
-		System.out.println("Tu nombre es: " + nombre);
+		// Si es de tercero y tiene sobresaliente, o es mayor de edad se mostrará el
+		// mensaje
+		// "APROBADO CON EXCELENCIA"
+
+		if ((curso.toLowerCase().equals("tercero") && notaAlumno >= 9) || edad >= 18) {
+			JOptionPane.showMessageDialog(null, "Aprobado con Excelencia", "Felicidades",
+					JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		// Si la nota es menor de 5 ha suspendido
+		if (notaAlumno < 5) {
+			mensaje = "Has Suspendido\n";
+		} else {
+			// Dentro de este else la nota sabemos que es 5 o más
+			if (notaAlumno < 6) {
+				mensaje = "Has sacado un Suficiente\n";
+			} else {
+				// LA nota es 6 o mayor de 6 si entra aqui
+				if (notaAlumno < 7) {
+					mensaje = "Has sacado un Bien\n";
+				} else {
+					// Si entramos aqui la nota es 7 o más
+					if (notaAlumno < 9) {
+
+						mensaje = "Has sacado un Notable\n";
+					} else {
+						// Si la nota es 9 o superior
+
+						// A todos los que tengan sobresaliente mayores de 14 años tendrán beca
+
+						if (edad >= 14) {
+							JOptionPane.showMessageDialog(null,
+									"Has conseguido la Beca!\n(Ya te puedes comprar el super ordenador gamer!!)",
+									"Felicidades", JOptionPane.INFORMATION_MESSAGE);
+						}
+						mensaje = "Has sacado un Sobresaliente\n";
+
+					}
+				}
+
+			}
+		}
+
+		System.out.println(mensaje);
 
 	}
 
