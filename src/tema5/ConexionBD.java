3f5ab654b3439f6fcfcb04fa30536a40df9ef007
blame110 <46011863+blame110@users.noreply.github.com>
2022-03-23 13:04:27 +0100

    23 marzo

--- src/tema5/ConexionBD.java
+++ src/tema5/ConexionBD.java
@@ -8,89 +8,38 @@ import java.util.Scanner;
 
 public class ConexionBD {
 
-	/*
-	 * Siempre que utilizamos un servicio necesitamos
-	 * Conectarnos al servicio, para eso es esta clase
+	/**
+	 * Funcion que se conecta a la BD de mysql y nos devuelve la conexion
 	 * 
-	 * Se necesitan siempre 4 parametros para conectarnos a un servicio
-	 * ip o nombre de dominio: localhost en nuestro caso que es nuestra maquina
-	 * usuario:root
-	 * password:toor (en vuestro caso "");
-	 * puerto de comunicaciones del servicio:3306 para mysql
-	 * Base de datos o esquema a utilizar
-	 * 
+	 * @return Una conexion activa a la bd o null si no se pudo conectar
 	 */
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//Parametros para la conexion a BD
-		String user= "root";
+	public static Connection conectarBD() {
+
+		// Parametros para la conexion a BD
+		String user = "root";
 		String password = "toor";
 		String url = "jdbc:mysql://localhost:3306/grandslam";
-		//Conexion para acceder a BD
+		// Conexion para acceder a BD
 		Connection con = null;
-		
-		Scanner teclado = new Scanner(System.in);
-		
+
 		try {
 			
-			//Nos conectamos a la Base de Datos
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Nos conectamos a la Base de Datos
 			con = DriverManager.getConnection(url, user, password);
-			
-			//Pedimos los datos del jugador
-			System.out.println("Introduce el id:");
-			int id = teclado.nextInt();
-			
-			System.out.println("Nombre:");
-			String nombre = teclado.next();
-	
-			System.out.println("Introduce la edad:");
-			int edad = teclado.nextInt();
 
-			System.out.println("Introduce la altura:");
-			int altura = teclado.nextInt();
-
-			System.out.println("Sexo:");
-			String sexo = teclado.next();
-			
-			//Consulta a ejecutar
-			String query = "insert into jugador values (?,?,?,?,?)";
-			
-			//El preparedStatement lo genera la conexion utilizando
-			//la consulta/query a ejecutar
-			PreparedStatement pStmt = con.prepareStatement(query);
-			
-			pStmt.setInt(1,id);
-			pStmt.setString(2, nombre);
-			pStmt.setInt(3, edad);
-			pStmt.setInt(4, altura);
-			pStmt.setString(5,sexo);
-			
-			//Ejecutamos la query
-			pStmt.execute();
-			
-			//Cerramos el statement
-			pStmt.close();
-			
-			
-		} catch (SQLException e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
-		//Cerramos la conexion
-		try {
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		teclado.close();
+
+		return con;
 	}
 
+
+	
+	
+	
+	
 }
