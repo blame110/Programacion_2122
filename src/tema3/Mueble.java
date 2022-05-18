7518cd8cb4f05ae94cffcb96d1ef7734ac32d576
blame110 <prueba@gmail.com>
2022-05-18 11:18:15 +0200

    Update Mueble.java

--- src/tema3/Mueble.java
+++ src/tema3/Mueble.java
@@ -6,6 +6,31 @@ public class Mueble {
 	private int peso;
 	private String Descripcion;
 	private int seccion;
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public int getSeccion() {
+		return seccion;
+	}
+	public void setSeccion(int seccion) {
+		this.seccion = seccion;
+	}
 
+	
 	
 }
